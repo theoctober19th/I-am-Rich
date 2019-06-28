bfe669ef8f190fdaa7311f80ba495333a01f4572
Bikalpa Dhakal <theoctober19th@gmail.com>
2019-06-28 13:14:59 +0545

    app completed

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Bikalpa Dhakal on 6/4/19.
+//  Copyright © 2019 Bikalpa Dhakal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
